fa241f408f8b9d75b6ba3b8e8f68ee7b9d65152d
miaoshaodong <miaoshaodong@ruijie.com.cn>
2019-07-29 14:42:44 +0800

    miado

--- src/main/java/org/springboot/SpringBootAjaxApplication.java
+++ src/main/java/org/springboot/SpringBootAjaxApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootAjaxApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootAjaxApplication.class, args);
+		System.out.println("111111sda");
 	}
 }
